161cbd87fe2f4b5386a540fb03969d143e653e2e
psiegmund <patrick-siegmund@web.de>
2022-04-28 23:18:55 +0200

    90%

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let inputFirstname = document.getElementById('input-name');
+let inputEmail = document.getElementById('input-email');
+let inputMsg = document.getElementById('input-text');
+
+
+function submitForm() {
+    console.log(inputFirstname.value);
+    console.log(inputEmail.value);
+    console.log(inputMsg.value);
+}
